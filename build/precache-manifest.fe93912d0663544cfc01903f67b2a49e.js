144aba0199cd022821d781e4659e123c65e65c27
vikas kumar <vikas10145.12@bitmesra.ac.in>
2020-09-03 20:59:16 +0530

    Intial commit
    initail build commit

---
+++ build/precache-manifest.fe93912d0663544cfc01903f67b2a49e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca3051cf889d90bbd9336f130909e418",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d0adf1e604c00fdded6",
+    "url": "/static/css/main.c7f978ad.chunk.css"
+  },
+  {
+    "revision": "5fcb08f47e755fb3df64",
+    "url": "/static/js/2.1a9d9cba.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1a9d9cba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d0adf1e604c00fdded6",
+    "url": "/static/js/main.38f54f18.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
